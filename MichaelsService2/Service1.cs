072dd4d5710b674b277954f9162e81f1aae8f86a
Michael-Swartz <51866528+Michael-Swartz@users.noreply.github.com>
2019-10-17 18:45:03 -0400

    Initial Save

---
+++ MichaelsService2/Service1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Net;
+using System.Net.Sockets;
+using OpenHardwareMonitor.Hardware;
+
+
+namespace MichaelsService2
+{
+    public partial class Service1 : ServiceBase
+    {
+        Timer timer = new Timer();
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+
+        public class UpdateVisitor : IVisitor 
+        {
+            public void VisitComputer(IComputer computer)
+            {
+                computer.Traverse(this);
+            }
+
+            public void VisitHardware(IHardware hardware)
+            {
+                hardware.Update();
+                foreach (IHardware subHardware in hardware.SubHardware) subHardware.Accept(this);
+            }
+            public void VisitSensor(ISensor sensor) { }
+            public void VisitParameter(IParameter parameter) { }
+
+
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WriteToFile("Service is Started at " + DateTime.Now);
+            
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval += 1500;
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WriteToFile("Service is stopped at " + DateTime.Now);
+        }
+
+        public static string format_string(string input)
+        {
+            if (input.Length == 3)
+            {
+                return input;
+            }
+
+            if (input.Length == 2)
+            {
+                string new_input = " " + input;
+                return new_input;
+            }
+
+            if (input.Length == 1)
+            {
+                string new_input = " " + input;
+                return new_input;
+            }
+
+            else
+            {
+                string new_input = "   ";
+                return new_input;
+            }
+
+
+        }
+
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            IPAddress broadcast = IPAddress.Parse("10.0.0.164");
+
+
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer();
+            computer.Open();
+            computer.CPUEnabled = true;
+            computer.Accept(updateVisitor);
+            string cput = "0";
+            string cpuu = "0";
+
+            for (int i = 0; i < computer.Hardware.Length; i++)
+            {
+                //GET CPU TEMP AND UTILIZATION 
+                if (computer.Hardware[i].HardwareType == HardwareType.CPU) ;
+                {
+                    for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
+                    {
+                        //GET CPU TEMP
+                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature)
+                        {
+                            if (computer.Hardware[i].Sensors[j].Name == "CPU Package")
+                            {
+                                //Console.WriteLine(computer.Hardware[i].Sensors[j].Name + ": " + format_string(computer.Hardware[i].Sensors[j].Value.ToString()) + "\r");
+                                cput = format_string(computer.Hardware[i].Sensors[j].Value.ToString());
+
+                            }
+                        }
+
+                        //GET CPU UTILIZATION
+                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Load)
+                        {
+
+
+                                //Console.WriteLine(computer.Hardware[i].Sensors[j].Name + ": " + computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                               
+                            cpuu = format_string(computer.Hardware[i].Sensors[j].Value.ToString());
+                               
+
+
+                               
+
+                            
+                        }
+                    }
+                }
+                //GET GPY TEMP AND UTILIZATION
+                if (computer.Hardware[i].HardwareType == HardwareType.GpuNvidia)
+                {
+                    for (int j = 0; j < computer.Hardware[i].Sensors.Length; j++)
+                    {
+                        //GET CPU TEMP
+                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Temperature)
+                        {
+
+                            // Console.WriteLine(computer.Hardware[i].Sensors[j].Name + ": " + computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                            // cput = computer.Hardware[i].Sensors[j].Value.ToString();
+                        }
+
+                        //GET CPU UTILIZATION
+                        if (computer.Hardware[i].Sensors[j].SensorType == SensorType.Load)
+                        {
+
+                            if (computer.Hardware[i].Sensors[j].Name == "CPU Total")
+                            {
+                                //Console.WriteLine(computer.Hardware[i].Sensors[j].Name + ": " + computer.Hardware[i].Sensors[j].Value.ToString() + "\r");
+                                // cpuu = computer.Hardware[i].Sensors[j].Value.ToString();
+
+                            }
+                        }
+                    }
+                }
+
+
+
+            }
+
+            string cputotal = cput + cpuu;
+
+            computer.Close();
+
+            string packet_data = cputotal;
+            byte[] sendbuff = Encoding.ASCII.GetBytes(packet_data);
+            IPEndPoint ep = new IPEndPoint(broadcast, 6666);
+            s.SendTo(sendbuff, ep);
+            //Random rnd = new Random();
+            //int num = rnd.Next();
+            // WriteToFile("Sent packet " + packet_data);
+
+            //string packet_data = GetRandomNumber(0, 234957675).ToString();
+
+
+
+
+        }
+
+
+        private static readonly Random getrandom = new Random();
+
+        public static int GetRandomNumber(int min, int max)
+        {
+            lock (getrandom)
+            {
+                return getrandom.Next(min, max);
+            }
+        }
+
+
+
+
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+           
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\Service" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+            
+            if (File.Exists(filepath)) 
+            {
+                using (StreamWriter sw = File.CreateText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }    
+            } 
+
+            else
+            {
+                using(StreamWriter sw = File.AppendText(filepath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+        }
+    }
+}
---
+++ monitor1_0/monitor1_0.ino
@@ -0,0 +1,139 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+#include <LiquidCrystal_I2C.h>
+#include <WiFiManager.h>
+
+
+
+WiFiServer server(80);
+//const char ssid[] = "Michael's Wifi";
+//const char pass[] = "mustangsallys";
+const int button = 16;
+WiFiUDP Udp;
+unsigned int localUdpPort = 6666;
+char incomingPacket[255];
+char replyPacket[] = "Hi i got your shit"; 
+long randNumber;
+int lcdColumns = 16;
+int lcdRows = 2;
+
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+
+
+
+void configModeCallback(WiFiManager *myWifiManager) {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("AP=PC Monitor");
+  lcd.setCursor(0,1);
+  lcd.print("URL=192.168.4.1");
+  Serial.println("CONFIGMODE");
+}
+
+void setup()
+{
+  
+  bool fresh_boot = false; 
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  Serial.begin(115200);
+  Serial.print("ON");
+  Serial.println();
+  pinMode(button, INPUT);
+  WiFiManager wifiManager;
+  
+  lcd.print("Press button for");
+  lcd.setCursor(0,1);
+  lcd.print("Wifi mode");
+  for (int i = 0; i < 4; i++) {
+    Serial.println(digitalRead(button));
+    if (!digitalRead(button)){
+      lcd.clear();
+      lcd.setCursor(0,0);
+      lcd.print("AP=PC Monitor");
+      lcd.setCursor(0,1);
+      lcd.print("URL=192.168.4.1");
+      Serial.println("BUTTON PRESSED");
+      wifiManager.resetSettings();
+  
+      break;
+    }
+    delay(2000);
+  }
+  
+
+wifiManager.setAPCallback(configModeCallback);
+  
+wifiManager.autoConnect("PC Monitor");
+
+
+
+
+
+ 
+  lcd.clear();
+  lcd.print("Connected to");
+  lcd.setCursor(0,1);
+  lcd.print("Wifi");
+  delay(4000);
+  //WiFi.begin(ssid, pass);
+//  lcd.print("Conecting to: " + String(ssid));
+//  Serial.print("Connecting");
+//  while (WiFi.status() != WL_CONNECTED)
+//  {
+//    delay(500);
+//    Serial.print(".");
+//  }
+//  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  lcd.clear();
+  Udp.begin(localUdpPort);
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
+  lcd.setCursor(0,0);
+  lcd.print("CPU T:    U:");
+  lcd.setCursor(0,1);
+  lcd.print("GPU T:");
+}
+
+
+void loop() 
+{
+
+  int packetSize = Udp.parsePacket();
+  if (packetSize)
+
+  {
+    Serial.printf("Received %d bytes from %s, port %d\n", packetSize, Udp.remoteIP().toString().c_str(), Udp.remotePort());
+    int len = Udp.read(incomingPacket, 255);
+    if (len > 0) 
+    {
+      incomingPacket[len] = '\0'; 
+    }
+
+    Serial.printf("Receieved: %s\n", incomingPacket);
+    for(int i = 0; i < 5 ; i++) {
+      Serial.println(incomingPacket[i]);   
+    }
+    
+    String cput = String(incomingPacket[0]) + String(incomingPacket[1]) + String(incomingPacket[2]);
+    String cpuu = String(incomingPacket[3]) + String(incomingPacket[4]) + String(incomingPacket[5]);
+    Serial.println("CPUT: " + cput);
+    
+    
+    Serial.printf("UDP Packet Contents: %s\n", incomingPacket);
+    Udp.beginPacket('10.0.0.255', '6666');
+    Udp.write(replyPacket);
+    Udp.endPacket();
+    lcd.setCursor(6,0);
+    lcd.print(String(cput));
+    lcd.setCursor(12,0);
+    lcd.print(String(cpuu));
+    Serial.printf("SENT PACKET");
+
+  } 
+  
+}
